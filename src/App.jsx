fd2c13332f95623bc8b71bcb568838b68fe057f1
Abrham007 <abrhamaraya111@gmail.com>
2024-02-12 18:00:42 +0300

    started the app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
 export default function App() {
-  return <div></div>;
+  return (
+    <main className="bg-11 w-screen h-screen pt-[78px]">
+      <div className="w-[100px] mx-auto flex flex-col gap-[70px] bg-1">
+        <header className="w-full flex justify-between">
+          <div className="flex flex-col gap-[6px]">
+            <h1 className="text-3xl text-8 tracking-tighter">Invoices</h1>
+            <p className="text-sm text-6">There are 7 total invoices</p>
+          </div>
+          <div>
+            <button>Filter by status</button>
+            <button>New Invoice</button>
+          </div>
+        </header>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      1: "#7C5DFA",
+      2: "#9277FF",
+      3: "#1E2139",
+      4: "#252945",
+      5: "#DFE3FA",
+      6: "#888EB0",
+      7: "#7E88C3",
+      8: "#0C0E16",
+      9: "#EC5757",
+      10: "#FF9797",
+      11: "#F8F8FB",
+      12: "#141625",
+    },
     extend: {},
   },
   plugins: [],
