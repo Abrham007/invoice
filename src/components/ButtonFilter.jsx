b78ed56b4b5e3519206f439fc8ae6cb77a6b9ab0
Abrham007 <abrhamaraya111@gmail.com>
2024-02-13 14:27:17 +0300

    removed the component from header and put it in it's on file

---
+++ src/components/ButtonFilter.jsx
@@ -0,0 +1,21 @@
+import downIcon from "../assets/icon-arrow-down.svg";
+export default function ButtonFilter() {
+  return (
+    <div className="relative self-center">
+      <button className="flex gap-[14px] items-center text-8 dark:text-white text-base font-bold ">
+        <span>Filter by status</span>
+        <img src={downIcon} alt=""></img>
+      </button>
+      <menu className="absolute invisible top-8 left-[-30px] w-[192px] flex flex-col gap-2 p-6 rounded-lg bg-white dark:bg-4 shadow-lg">
+        <li>
+          <label className="group">
+            <input type="checkbox" className="absolute w-0 h-0 opacity-0 peer"></input>
+            <span className="flex gap-3 items-center before:block before:w-4 before:h-4 before:rounded-sm before:bg-5 group-hover:before:border-1 group-hover:before:border-[1px] group-hover:before:border-solid group-hover:cursor-pointer peer-checked:before:bg-1 peer-checked:before:bg-checkIcon peer-checked:before:bg-no-repeat peer-checked:before:bg-center text-8 dark:text-white text-[15px] font-bold">
+              Draft
+            </span>
+          </label>
+        </li>
+      </menu>
+    </div>
+  );
+}
