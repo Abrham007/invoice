11d5637358bf016715fd14ecda077b70bab378e7
Abrham007 <abrhamaraya111@gmail.com>
2024-02-15 10:06:19 +0300

    removed a section to it's own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
+import Main from "./components/Main";
+import SideBar from "./components/SideBar";
+
 export default function App() {
   return (
-    <main className="bg-11 w-screen h-screen pt-[78px]">
-      <div className="w-[100px] mx-auto flex flex-col gap-[70px] bg-1">
-        <header className="w-full flex justify-between">
-          <div className="flex flex-col gap-[6px]">
-            <h1 className="text-3xl text-8 tracking-tighter">Invoices</h1>
-            <p className="text-sm text-6">There are 7 total invoices</p>
-          </div>
-          <div>
-            <button>Filter by status</button>
-            <button>New Invoice</button>
-          </div>
-        </header>
-        <ul>
-          <li></li>
-        </ul>
-      </div>
-    </main>
+    <div className="w-screen h-screen bg-11 dark:bg-12  py-[108px] md:py-[142px] lg:py-[78px]">
+      <SideBar></SideBar>
+      <Main></Main>
+    </div>
   );
 }
