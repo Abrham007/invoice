577926c5a50a2da689b2877e352e2be41d8bbc50
Abrham007 <abrhamaraya111@gmail.com>
2024-02-16 17:56:39 +0300

    added a custom shadow to the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -57,6 +57,7 @@ module.exports = {
     },
     boxShadow: {
       DEFAULT: "0px 10px 10px -10px rgba(72, 84, 159, 0.10)",
+      lg: "0px 10px 20px 0px rgba(72, 84, 159, 0.25)",
     },
 
     extend: {
