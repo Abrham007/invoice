0f674ecbd9e0057174a6ba01351edbd5540c672d
Abrham007 <abrhamaraya111@gmail.com>
2024-02-19 18:00:47 +0300

    improved the UX

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,6 +1,6 @@
 export default function InputText({ label, type = "text", ...props }) {
   return (
-    <label className="w-full flex flex-col gap-[9px]">
+    <label className="w-full flex flex-col gap-[9px] cursor-pointer">
       <p className="w-full flex justify-between">
         <span className="text-sm text-7 dark:text-5">{label}</span>
         <span className="hidden text-[0.625rem] font-semibold leading-[1.5] text-9">
@@ -9,7 +9,7 @@ export default function InputText({ label, type = "text", ...props }) {
       </p>
 
       <input
-        className="w-full pt-[18px] px-5 pb-[15px] text-base text-8 dark:text-white rounded bg-white outline-none border border-5 hover:border-[#9277FF] "
+        className="w-full pt-[18px] px-5 pb-[15px] text-base text-8 dark:text-white rounded bg-white dark:bg-3 outline-none border border-5 dark:border-[#252945] hover:border-[#9277FF] cursor-pointer"
         type={type}
         {...props}
       ></input>
