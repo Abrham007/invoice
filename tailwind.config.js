f58f1bd3aead1fa3ece5a500787f26c50aa4f216
Abrham007 <abrhamaraya111@gmail.com>
2024-02-19 18:03:15 +0300

    added a lagrger shadow for the footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,6 +58,7 @@ module.exports = {
     boxShadow: {
       DEFAULT: "0px 10px 10px -10px rgba(72, 84, 159, 0.10)",
       lg: "0px 10px 20px 0px rgba(72, 84, 159, 0.25)",
+      xl: "0px -30px 30px 0px rgba(72, 84, 159, 0.10)",
     },
 
     extend: {
