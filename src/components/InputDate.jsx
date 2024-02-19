9f3c1d3b6fbd318bd0996f5b148c05bf29248185
Abrham007 <abrhamaraya111@gmail.com>
2024-02-19 17:10:15 +0300

    finished styling the this component

--- src/components/InputDate.jsx
+++ src/components/InputDate.jsx
@@ -1,25 +1,70 @@
 import * as React from "react";
+import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import IconCalendar from "./IconCalendar";
 
-export default function InputDate() {
+export default function InputDate({ lable, disabled = false }) {
+  let isDarkMode = document.documentElement.classList.contains("dark");
+  let color = "#0C0E16";
+  let backgroundColor = "#FFF";
+  let borderColor = "#DFE3FA";
+
+  if (isDarkMode) {
+    color = "#FFF";
+    backgroundColor = "#1E2139";
+    borderColor = "#252945";
+  }
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <label className="w-full flex flex-col gap-[9px]">
-        <span className="text-sm text-7 dark:text-5">Issue Date</span>
+        <span className="text-sm text-7 dark:text-5">{lable}</span>
 
-        <DesktopDatePicker
+        <DatePicker
+          disabled={disabled}
+          className="cursor-pointer"
+          format="DD MMM YYYY"
+          defaultValue={dayjs("2022-08-17")}
           slots={{ openPickerIcon: IconCalendar }}
           slotProps={{
             openPickerButton: {
               className: "w-[40px] h-[40px]",
             },
+            layout: {
+              sx: {
+                backgroundColor: `${backgroundColor}`,
+                color: `${color}`,
+                "& .MuiPickersDay-root": {
+                  color: `${color}`,
+                },
+                "& .MuiPickersCalendarHeader-switchViewButton": {
+                  color: `${color}`,
+                },
+                "& .MuiTypography-root": {
+                  color: `${color}`,
+                },
+                "& .MuiSvgIcon-root": {
+                  color: `${color}`,
+                },
+              },
+            },
           }}
           sx={{
+            "& .MuiDateCalendar-root": {
+              borderRadius: "8px",
+            },
+            "& .MuiOutlinedInput-root": {
+              backgroundColor: `${backgroundColor}`,
+              color: `${color}`,
+              fontFamily: "League Spartan, sans-serif",
+              fontSize: "0.9375rem",
+              lineHeight: "0.9375rem",
+              letterSpacing: "-0.01563",
+              fontWeight: "bold",
+            },
             "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-              border: "1px solid #DFE3FA",
+              border: `1px solid ${borderColor}`,
             },
             "& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline": {
               border: "1px solid #9277FF",
