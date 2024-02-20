d7e979dee9f3787c1a191d65871313daaccd7077
Abrham007 <abrhamaraya111@gmail.com>
2024-02-20 14:53:11 +0300

    made the component responsive for tablet and desktop

--- src/components/AddEditInvoice/InvoiceInfo.jsx
+++ src/components/AddEditInvoice/InvoiceInfo.jsx
@@ -4,8 +4,11 @@ import InputText from "../InputText";
 export default function InvoiceInfo() {
   return (
     <fieldset className="flex flex-col gap-6">
-      <InputDate lable="Invoice Date"></InputDate>
-      <InputDropdown label="Payment Terms"></InputDropdown>
+      <div className="flex flex-col md:flex-row gap-6">
+        <InputDate lable="Invoice Date"></InputDate>
+        <InputDropdown label="Payment Terms"></InputDropdown>
+      </div>
+
       <InputText label="Project Description"></InputText>
     </fieldset>
   );
