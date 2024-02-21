7a04523c23b2e1792508301126bd3460f7522cf3
Abrham007 <abrhamaraya111@gmail.com>
2024-02-21 12:55:38 +0300

    corrected the layout bug

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,7 @@ import InvoiceList from "./InvoiceList";
 
 export default function Main() {
   return (
-    <main className="w-[327px] md:w-[672px] lg:w-[730px] h-full mx-auto flex flex-col item-center gap-[32px] md:gap-[55px] lg:gap-[70px]">
+    <main className="w-[327px] md:w-[672px] lg:w-[730px] min-h-screen py-[108px] md:py-[142px] lg:py-[78px] mx-auto flex flex-col item-center gap-[32px] md:gap-[55px] lg:gap-[70px]">
       <Header></Header>
       <InvoiceList></InvoiceList>
       {/* <InvoiceListEmpty></InvoiceListEmpty> */}
