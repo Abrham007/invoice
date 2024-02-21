11ae9ae306f56e865bc2e8e72a96ceab742863db
Abrham007 <abrhamaraya111@gmail.com>
2024-02-21 12:07:09 +0300

    made the component more reusable

--- src/components/AddEditInvoice/Item.jsx
+++ src/components/AddEditInvoice/Item.jsx
@@ -1,23 +1,54 @@
 import InputText from "../InputText";
 import deleteIcon from "../../assets/icon-delete.svg";
+import { useMediaQuery } from "react-responsive";
+import IconDelete from "../IconDelete";
+
 export default function Item() {
+  const isBigScreen = useMediaQuery({ query: "(min-width: 768px)" });
+  let inputCss =
+    "w-full pt-[18px] px-5 pb-[15px] text-base text-8 dark:text-white rounded bg-white dark:bg-3 outline-none border border-5 dark:border-[#252945] hover:border-[#9277FF] cursor-pointer";
+
   return (
     <fieldset className="flex flex-col md:flex-row gap-[15px]">
-      <InputText label="Item Name"></InputText>
+      {isBigScreen ? (
+        <input
+          className={inputCss}
+          type="text"
+          aria-labelledby="itemLabel"
+        ></input>
+      ) : (
+        <InputText label="Item Name"></InputText>
+      )}
       <fieldset className="w-full flex gap-4">
         <span className="w-[64px]">
-          <InputText label="Qty."></InputText>
+          {isBigScreen ? (
+            <input
+              className={inputCss}
+              type="text"
+              aria-labelledby="qtyLabel"
+            ></input>
+          ) : (
+            <InputText label="Qty."></InputText>
+          )}
         </span>
         <span className="w-[100px]">
-          <InputText label="Price"></InputText>
+          {isBigScreen ? (
+            <input
+              className={inputCss}
+              type="text"
+              aria-labelledby="priceLabel"
+            ></input>
+          ) : (
+            <InputText label="Price"></InputText>
+          )}
         </span>
-        <span className="w-[100px] flex flex-col gap-[27px]">
-          <span className="text-sm text-7 dark:text-5">Total</span>
+        <span className="w-[100px] flex flex-col gap-[27px] md:justify-center">
+          <span className="text-sm text-7 dark:text-5 md:hidden">Total</span>
           <span className="text-sm text-7 dark:text-5 font-bold">156.00</span>
         </span>
         <span className=" flex justify-end items-end pb-4">
-          <button>
-            <img src={deleteIcon} alt=""></img>
+          <button className="">
+            <IconDelete></IconDelete>
           </button>
         </span>
       </fieldset>
