ae69a8589b8ccf9164b6c87e101319bdd442f097
Abrham007 <abrhamaraya111@gmail.com>
2024-02-22 11:34:14 +0300

    corrected a logic bug

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,27 +1,32 @@
 export default function Status({ children }) {
   let styleOfDot = "w-2 h-2 rounded-full";
   let styleOfText = "text-base font-bold";
+  let text = "";
   switch (children) {
-    case "Paid":
+    case "paid":
       styleOfDot += " bg-[#33D69F]";
       styleOfText += " text-[#33D69F]";
+      text = "Paid";
       break;
-    case "Pending":
+    case "pending":
       styleOfDot += " bg-[#FF8F00]";
       styleOfText += " text-[#FF8F00]";
+      text = "Pending";
       break;
-    case "Draft":
+    case "draft":
       styleOfDot += " bg-[#373B53]";
       styleOfText += " text-[#373B53]";
+      text = "Draft";
       break;
     default:
       styleOfDot += " bg-[#33D69F]";
       styleOfText += " text-[#33D69F]";
+      text = "Paid";
   }
   return (
     <p className="md:mr-[20px] col-start-2 col-end-3 row-start-2 row-end-4 justify-self-end self-center h-[40px] w-[104px] bg-[#33D69F10]  rounded-md flex justify-center items-center gap-2">
       <span role="presentation" className={styleOfDot}></span>
-      <span className={styleOfText}>{children}</span>
+      <span className={styleOfText}>{text}</span>
     </p>
   );
 }
