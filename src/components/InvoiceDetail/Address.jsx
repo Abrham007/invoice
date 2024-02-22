92d7496c30d6a6336c6255bbc8161f6e9a169bda
Abrham007 <abrhamaraya111@gmail.com>
2024-02-22 11:35:00 +0300

    corrected a content bug

--- src/components/InvoiceDetail/Address.jsx
+++ src/components/InvoiceDetail/Address.jsx
@@ -1,9 +1,9 @@
-export default function Address({ street, city, zone, country }) {
+export default function Address({ street, city, postcode, country }) {
   return (
     <div className="flex flex-col gap-1">
       <p className="text-sm text-7 dark:text-5">{street}</p>
       <p className="text-sm text-7 dark:text-5">{city}</p>
-      <p className="text-sm text-7 dark:text-5">{zone}</p>
+      <p className="text-sm text-7 dark:text-5">{postcode}</p>
       <p className="text-sm text-7 dark:text-5">{country}</p>
     </div>
   );
