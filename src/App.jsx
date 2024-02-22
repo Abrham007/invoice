2a003c1025e742dbc11c0d0b90322925e16a74e4
Abrham007 <abrhamaraya111@gmail.com>
2024-02-22 11:33:23 +0300

    wrapped it with the data context

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import InvoiceDetail from "./components/InvoiceDetail/InvoiceDetail";
 import Main from "./components/Main/Main";
 import Modal from "./components/Modal";
 import SideBar from "./components/SideBar";
+import DataContextProvider from "./store/DataContext";
 
 export default function App() {
   return (
-    <div className="w-full h-full bg-11 dark:bg-12">
-      <SideBar></SideBar>
-      <Main></Main>
-      {/* <InvoiceDetail></InvoiceDetail> */}
-      {/* <AddEditInvoice></AddEditInvoice> */}
-      {/* <Modal>
+    <div className="w-full min-h-screen bg-11 dark:bg-12">
+      <DataContextProvider>
+        <SideBar></SideBar>
+        {/* <Main></Main> */}
+        {/* <InvoiceDetail></InvoiceDetail> */}
+        <AddEditInvoice></AddEditInvoice>
+        {/* <Modal>
         <Delete></Delete>
       </Modal> */}
+      </DataContextProvider>
     </div>
   );
 }
