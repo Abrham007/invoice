82230096b92cd66a5e097f5e2133c30a1e5c3075
Abrham007 <abrhamaraya111@gmail.com>
2024-02-23 10:25:45 +0300

    improved the code style

--- src/components/Main/Invoice.jsx
+++ src/components/Main/Invoice.jsx
@@ -6,24 +6,24 @@ export default function Invoice(props) {
   //   dateStyle: "medium",
   // }).format(new Date(props.paymentDue));
   return (
-    <button className="h-[134px] md:h-[72px] w-full p-6 md:p-0 grid grid-cols-[1fr_1fr] grid-rows-[2fr_1fr_1fr] gap-[9px] justify-items-start  md:flex md:gap-0 md:justify-center md:items-center bg-white dark:bg-3 rounded-lg shadow hover:border-[1px] hover:border-solid hover:border-1">
-      <p className="md:mr-[28px] lg:mr-[44px] text-8 dark:text-white text-base font-bold">
+    <button className="h-[134px] md:h-[72px] w-full p-6  grid grid-cols-[1fr_1fr] grid-rows-[2fr_1fr_1fr] gap-[9px] justify-items-start  md:flex md:gap-[30px] md:justify-start md:items-center md:text-left bg-white dark:bg-3 rounded-lg shadow hover:border-[1px] hover:border-solid hover:border-1">
+      <span className=" text-8 dark:text-white text-base font-bold md:flex-1">
         <span className="text-7 font-bold text-base">#</span>
         {props.id}
-      </p>
-      <p className="md:mr-[51px] lg:mr-[59px] col-start-1 col-end-2 row-start-2 row-end-3 self-end md:self-center text-7 dark:text-5 text-sm">
+      </span>
+      <span className="col-start-1 col-end-2 row-start-2 row-end-3 self-end text-7 dark:text-5 text-sm  md:self-center">
         Due {date}
-      </p>
-      <p className="md:mr-[82px] lg:mr-[108px] col-start-2 col-end-3 row-start-1 row-end-2 justify-self-end self-center text-7 dark:text-white text-sm">
+      </span>
+      <span className="col-start-2 col-end-3 row-start-1 row-end-2 justify-self-end self-center text-7 dark:text-white text-sm md:flex-1 ">
         {props.clientName}
-      </p>
-      <p className="md:mr-[40px] col-start-1 col-end-2 row-start-3 row-end-4 self-end md:self-center text-8 dark:text-white text-base/6 font-bold">
+      </span>
+      <span className="col-start-1 col-end-2 row-start-3 row-end-4 self-end md:self-center text-8 dark:text-white text-base/6 font-bold md:flex-1 md:text-right ">
         £ {props.total}
-      </p>
+      </span>
       <Status>Paid</Status>
-      <p className="hidden md:inline-block">
+      <span className="hidden md:inline-block">
         <img src={rightIcon} alt=""></img>
-      </p>
+      </span>
     </button>
   );
 }
