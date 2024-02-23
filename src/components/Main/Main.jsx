f5cf2c01f583754c5e296995f5f2bd6b9f6622a6
Abrham007 <abrhamaraya111@gmail.com>
2024-02-23 17:03:40 +0300

    added the filter functionalty

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,37 @@
 import InvoiceListEmpty from "./InvoiceListEmpty";
 import Header from "./Header";
 import InvoiceList from "./InvoiceList";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DataContext } from "../../store/DataContext";
 
 export default function Main() {
   const { data } = useContext(DataContext);
+  const [invoiceList, setinvoiceList] = useState(data);
+
+  function handleFilterStatus(e) {
+    switch (e.target.value) {
+      case "all":
+        setinvoiceList(data);
+        break;
+      case "draft":
+        setinvoiceList(data.filter((invoice) => invoice.status === "draft"));
+        break;
+      case "pending":
+        setinvoiceList(data.filter((invoice) => invoice.status === "pending"));
+        break;
+      case "paid":
+        setinvoiceList(data.filter((invoice) => invoice.status === "paid"));
+        break;
+      default:
+        setinvoiceList(data);
+    }
+  }
 
   return (
     <main className="w-[327px] md:w-[672px] lg:w-[730px] min-h-screen py-[108px] md:py-[142px] lg:py-[78px] mx-auto flex flex-col item-center gap-[32px] md:gap-[55px] lg:gap-[70px]">
-      <Header></Header>
+      <Header onChange={handleFilterStatus}></Header>
       {data.length !== 0 ? (
-        <InvoiceList data={data}></InvoiceList>
+        <InvoiceList data={invoiceList}></InvoiceList>
       ) : (
         <InvoiceListEmpty></InvoiceListEmpty>
       )}
