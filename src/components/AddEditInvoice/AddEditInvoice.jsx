de5aaf2ef2ed104eb5159f2efbe86379aebc295f
Abrham007 <abrhamaraya111@gmail.com>
2024-02-24 12:38:51 +0300

    made the component reusable

--- src/components/AddEditInvoice/AddEditInvoice.jsx
+++ src/components/AddEditInvoice/AddEditInvoice.jsx
@@ -4,30 +4,47 @@ import BillTo from "./BillTo";
 import InvoiceInfo from "./InvoiceInfo";
 import ItemList from "./ItemList";
 import leftIcon from "../../assets/icon-arrow-left.svg";
-import Wrapper from "./Wrapper";
 import { useState } from "react";
+import Modal from "../Modal";
 
-export default function AddEditInvoice({ isOpen, handleClose }) {
+export default function AddEditInvoice({ isOpen, handleClose, id }) {
+  let title = (
+    <h2 className="text-lg/8 text-8 dark:text-white font-bold">New Invoice</h2>
+  );
+
+  if (id) {
+    title = (
+      <h2 className="text-lg/8 text-8 dark:text-white font-bold">
+        Edit <span className="text-6">#</span>
+        {id}
+      </h2>
+    );
+  }
   function closeInvoice() {
     handleClose();
   }
   return (
-    <Wrapper isOpen={isOpen}>
+    <Modal
+      className="min-h-screen max-w-[100%] md:w-[616px] pb-[80px] lg:pb-[0px] md:rounded-br-[20px] md:rounded-tr-[20px]  backdrop:opacity-50 backdrop:bg-[#000] backdrop:top-[80px] top-[80px] lg:backdrop:top-[0px] lg:top-[0px] lg:backdrop:left-[104px] lg:left-[104px] bg-white dark:bg-12"
+      isOpen={isOpen}
+      setIsOpen={handleClose}
+    >
       <div className="p-6 pb-8 md:p-14 flex flex-col gap-[22px] bg-white dark:bg-12">
-        <a href="#" className="flex gap-6 items-start  self-start md:hidden">
+        <button
+          onClick={closeInvoice}
+          className="flex gap-6 items-start  self-start md:hidden"
+        >
           <img src={leftIcon} alt=""></img>
           <span className="text-base text-8 dark:text-white hover:text-7">
             Go Back
           </span>
-        </a>
-        <h2 className="text-lg/8 text-8 dark:text-white font-bold">
-          Edit <span className="text-6">#</span>XM9141
-        </h2>
+        </button>
+        {title}
         <div className=" flex flex-col gap-[41px] ">
-          <BillFrom></BillFrom>
-          <BillTo></BillTo>
-          <InvoiceInfo></InvoiceInfo>
-          <ItemList></ItemList>
+          <BillFrom id={id}></BillFrom>
+          <BillTo id={id}></BillTo>
+          <InvoiceInfo id={id}></InvoiceInfo>
+          <ItemList id={id}></ItemList>
         </div>
       </div>
       <div className="sticky bottom-0 left-0 w-screen md:w-full  p-6 flex gap-2 justify-end items-center bg-white dark:bg-3 shadow-xl md:rounded-r-[20px]">
@@ -36,6 +53,6 @@ export default function AddEditInvoice({ isOpen, handleClose }) {
         </Button>
         <Button type="2">Save Changes</Button>
       </div>
-    </Wrapper>
+    </Modal>
   );
 }
