b652d459aa306044d76ced8bb62f3ea17fcab64d
Abrham007 <abrhamaraya111@gmail.com>
2024-02-24 12:39:40 +0300

    added the InvoiceDetail functionalty

--- src/components/InvoiceDetail/ButtonGroup.jsx
+++ src/components/InvoiceDetail/ButtonGroup.jsx
@@ -1,10 +1,43 @@
+import { useRef, useState } from "react";
 import Button from "../UI/Buttons/Button";
-export default function ButtonGroup() {
+import Modal from "../Modal";
+import Delete from "../Delete";
+import AddEditInvoice from "../AddEditInvoice/AddEditInvoice";
+export default function ButtonGroup({ id }) {
+  const [isEditOpen, setEditOpen] = useState(false);
+  const [isDeleteOpen, setDeleteOpen] = useState(false);
+
+  function openEditInvoice() {
+    setEditOpen(true);
+  }
+
+  function openDelete() {
+    setDeleteOpen(true);
+  }
+
   return (
     <>
-      <Button type="3">Edit</Button>
-      <Button type="5">Delete</Button>
+      <Button type="3" onClick={openEditInvoice}>
+        Edit
+      </Button>
+      <Button type="5" onClick={openDelete}>
+        Delete
+      </Button>
       <Button type="2">Mark as Paid</Button>
+      <Modal
+        isOpen={isDeleteOpen}
+        setIsOpen={setDeleteOpen}
+        className="backdrop:opacity-50 backdrop:bg-[#000]"
+      >
+        <Delete></Delete>
+      </Modal>
+      {isEditOpen && (
+        <AddEditInvoice
+          id={id}
+          isOpen={isEditOpen}
+          handleClose={setEditOpen}
+        ></AddEditInvoice>
+      )}
     </>
   );
 }
