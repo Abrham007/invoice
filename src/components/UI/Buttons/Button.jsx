c57110ac1ad31c22cbc4f11f1c5d6bab8dbdf1ca
Abrham007 <abrhamaraya111@gmail.com>
2024-02-24 12:40:30 +0300

    corrected a style bug

--- src/components/UI/Buttons/Button.jsx
+++ src/components/UI/Buttons/Button.jsx
@@ -3,27 +3,27 @@ export default function Button({ children, type, ...props }) {
   switch (type) {
     case "1":
       cssClass =
-        "pl-[8px] pr-[17px] h-[48px] rounded-full bg-1 flex gap-4 justify-center items-center self-center text-white text-base font-bold hover:bg-2";
+        "pl-[8px] pr-[17px] h-[48px] rounded-full bg-1 flex gap-4 justify-center items-center self-center text-white text-base font-bold hover:bg-2 outline-none";
       break;
     case "2":
       cssClass =
-        "px-[23px] h-[48px] rounded-full text-white text-base bg-1 hover:bg-2 font-bold";
+        "px-[23px] h-[48px] rounded-full text-white text-base bg-1 hover:bg-2 font-bold outline-none";
       break;
     case "3":
       cssClass =
-        "px-[23px] h-[48px] rounded-full bg-[#F9FAFE] hover:bg-5 dark:bg-4 dark:hover:bg-white text-7 text-base dark:text-5";
+        "px-[23px] h-[48px] rounded-full bg-[#F9FAFE] hover:bg-5 dark:bg-4 dark:hover:bg-white text-7 text-base dark:text-5 outline-none";
       break;
     case "4":
       cssClass =
-        "px-[23px] h-[48px] rounded-full bg-[#373B53] hover:bg-8 dark:hover:bg-3 text-6 dark:text-5 text-base  font-bold";
+        "px-[23px] h-[48px] rounded-full bg-[#373B53] hover:bg-8 dark:hover:bg-3 text-6 dark:text-5 text-base  font-bold outline-none";
       break;
     case "5":
       cssClass =
-        "px-[23px] h-[48px] rounded-full bg-9 hover:bg-10 text-white text-base font-bold";
+        "px-[23px] h-[48px] rounded-full bg-9 hover:bg-10 text-white text-base font-bold outline-none";
       break;
     case "6":
       cssClass =
-        "w-full h-[48px] rounded-full bg-[#F9FAFE] hover:bg-5 text-7 text-base font-bold";
+        "w-full h-[48px] rounded-full bg-[#F9FAFE] hover:bg-5 text-7 text-base font-bold outline-none";
       break;
     default:
   }
