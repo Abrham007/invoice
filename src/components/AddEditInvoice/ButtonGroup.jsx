5c28c746a628b6eba9c43565028bbc92948e3ad9
Abrham007 <abrhamaraya111@gmail.com>
2024-02-26 18:13:17 +0300

    added a chiled component to lighten the parent

---
+++ src/components/AddEditInvoice/ButtonGroup.jsx
@@ -0,0 +1,33 @@
+import Button from "../UI/Buttons/Button";
+
+export default function ButtonGroup({ handleClose, id }) {
+  function closeInvoice() {
+    handleClose();
+  }
+
+  if (id) {
+    return (
+      <div className="sticky bottom-0 left-0 w-screen md:w-full  p-6 flex gap-2 justify-end items-center bg-white dark:bg-3 shadow-xl md:rounded-r-[20px]">
+        <Button $type="3" onClick={closeInvoice}>
+          Cancel
+        </Button>
+        <Button $type="2">Save Changes</Button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="sticky bottom-0 left-0 w-screen md:w-full  p-6 flex gap-2 justify-between items-center bg-white dark:bg-3 shadow-xl md:rounded-r-[20px]">
+        <Button $type="3" onClick={closeInvoice}>
+          Discard
+        </Button>
+
+        <div className="flex gap-2">
+          <Button $type="4">Save as Draft</Button>
+          <Button $type="2" form="invoiceForm" type="submit">
+            Save & Send
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
