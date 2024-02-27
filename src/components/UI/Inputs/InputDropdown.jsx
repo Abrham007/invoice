3d102aac5ed10a4ada28b76b461263161d510c01
Abrham007 <abrhamaraya111@gmail.com>
2024-02-27 17:28:56 +0300

    made component more functional

--- src/components/UI/Inputs/InputDropdown.jsx
+++ src/components/UI/Inputs/InputDropdown.jsx
@@ -5,11 +5,24 @@ export default function InputDropdown({
   defaultValue,
   value,
   onChange,
+  onBlur,
 }) {
-  const [selectedTerm, setSelectedTerm] = useState(defaultValue);
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleOpen() {
+    setIsOpen((prevValue) => !prevValue);
+  }
+
+  let menuStyles =
+    "absolute top-[80px] left-0 w-full list-none rounded-lg bg-white dark:bg-4 shadow-lg divide-y divide-5 dark:divide-3";
+
+  if (isOpen) {
+    menuStyles += " flex flex-col";
+  } else {
+    menuStyles += " hidden";
+  }
 
   function handleSelectingTerm(newTerm) {
-    setSelectedTerm(newTerm);
     onChange(newTerm);
   }
 
@@ -18,16 +31,17 @@ export default function InputDropdown({
       <p className="text-sm text-7 dark:text-5">{label}</p>
 
       <button
+        onClick={toggleOpen}
         type="button"
         className=" w-full pt-[20px] pl-5 pr-4 pb-[18px] flex items-center justify-between rounded bg-white dark:bg-3 border border-5 dark:border-[#1E2139] hover:border-[#9277FF] "
       >
         <span className="text-base text-8 dark:text-white">
-          Net {selectedTerm} {value} Days
+          Net {value} Days
         </span>
         <img src={downIcon} alt=""></img>
       </button>
 
-      <menu className="hidden group-hover:flex flex-col absolute top-[80px] left-0 w-full list-none rounded-lg bg-white dark:bg-4 shadow-lg divide-y divide-5 dark:divide-3">
+      <menu className={menuStyles}>
         <li>
           <button
             type="button"
