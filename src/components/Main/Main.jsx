02b0defbaaad298b6bb0c152d48d451cb058fd9c
Abrham007 <abrhamaraya111@gmail.com>
2024-02-27 13:31:01 +0300

    made the component interactive

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -55,6 +55,7 @@ export default function Main() {
   return (
     <main className="w-[327px] md:w-[672px] lg:w-[730px] min-h-screen py-[108px] md:py-[142px] lg:py-[78px] mx-auto flex flex-col item-center gap-[32px] md:gap-[55px] lg:gap-[70px]">
       <Header
+        data={invoiceList}
         onChange={handleFilterStatus}
         openAddInvoice={openAddInvoice}
       ></Header>
