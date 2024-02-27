d0bbc4dcd31f7982ece0ff29ff8edd1a2adf826f
Abrham007 <abrhamaraya111@gmail.com>
2024-02-27 13:30:08 +0300

    made the component work with react-hook-form library

--- src/components/UI/Inputs/InputDropdown.jsx
+++ src/components/UI/Inputs/InputDropdown.jsx
@@ -1,24 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import downIcon from "../../../assets/icon-arrow-down.svg";
-export default function InputDropdown({ label, defaultValue }) {
+export default function InputDropdown({
+  label,
+  defaultValue,
+  value,
+  onChange,
+}) {
   const [selectedTerm, setSelectedTerm] = useState(defaultValue);
 
   function handleSelectingTerm(newTerm) {
     setSelectedTerm(newTerm);
+    onChange(newTerm);
   }
 
   return (
     <div className="group w-full relative flex flex-col gap-[9px] cursor-pointer">
       <p className="text-sm text-7 dark:text-5">{label}</p>
-      <div className="w-full pt-[20px] pl-5 pr-4 pb-[18px] flex items-center justify-between rounded bg-white dark:bg-3 border border-5 dark:border-[#1E2139] hover:border-[#9277FF] ">
+
+      <button
+        type="button"
+        className=" w-full pt-[20px] pl-5 pr-4 pb-[18px] flex items-center justify-between rounded bg-white dark:bg-3 border border-5 dark:border-[#1E2139] hover:border-[#9277FF] "
+      >
         <span className="text-base text-8 dark:text-white">
-          Net {selectedTerm} Days
+          Net {selectedTerm} {value} Days
         </span>
         <img src={downIcon} alt=""></img>
-      </div>
+      </button>
+
       <menu className="hidden group-hover:flex flex-col absolute top-[80px] left-0 w-full list-none rounded-lg bg-white dark:bg-4 shadow-lg divide-y divide-5 dark:divide-3">
         <li>
           <button
+            type="button"
             onClick={() => handleSelectingTerm(1)}
             className="px-6 py-4 bg-transparent border-none outline-none text-base text-8 dark:text-white hover:text-1"
           >
@@ -27,6 +39,7 @@ export default function InputDropdown({ label, defaultValue }) {
         </li>
         <li>
           <button
+            type="button"
             onClick={() => handleSelectingTerm(7)}
             className="px-6 py-4 bg-transparent border-none outline-none text-base text-8 dark:text-white hover:text-1"
           >
@@ -35,6 +48,7 @@ export default function InputDropdown({ label, defaultValue }) {
         </li>
         <li>
           <button
+            type="button"
             onClick={() => handleSelectingTerm(14)}
             className="px-6 py-4 bg-transparent border-none outline-none text-base text-8 dark:text-white hover:text-1"
           >
@@ -43,6 +57,7 @@ export default function InputDropdown({ label, defaultValue }) {
         </li>
         <li>
           <button
+            type="button"
             onClick={() => handleSelectingTerm(30)}
             className="px-6 py-4 bg-transparent border-none outline-none text-base text-8 dark:text-white hover:text-1"
           >
