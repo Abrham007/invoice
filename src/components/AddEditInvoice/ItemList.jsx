383f1e330463b52f511f2732b787f7177f593282
Abrham007 <abrhamaraya111@gmail.com>
2024-02-27 17:28:06 +0300

    improved the code by connecting to the library

--- src/components/AddEditInvoice/ItemList.jsx
+++ src/components/AddEditInvoice/ItemList.jsx
@@ -3,24 +3,14 @@ import Item from "./Item";
 import { useContext, useState } from "react";
 import { DataContext } from "../../store/DataContext";
 
-export default function ItemList({ id }) {
-  const { data } = useContext(DataContext);
-  const currentInvoice = data.find((invoice) => invoice.id === id);
-  const tempItemList = currentInvoice?.items || [];
-  const [itemList, setItemList] = useState(tempItemList);
-
-  function addItem() {
-    setItemList((prevValue) => [...prevValue, {}]);
-  }
-
-  function removeItem(index) {
-    setItemList((prevValue) => {
-      let tempValue = [...prevValue];
-      tempValue.splice(index, 1);
-      return tempValue;
-    });
-  }
-
+export default function ItemList({
+  id,
+  fields,
+  append,
+  remove,
+  register,
+  errors,
+}) {
   return (
     <fieldset className="min-w-[100%] flex flex-col gap-[22px]">
       <h3 className="text-[1.125rem]/8 text-[#777F98] -tracking-[0.02344rem] font-bold">
@@ -43,13 +33,23 @@ export default function ItemList({ id }) {
           <span className="w-[56px] text-sm text-7 dark:text-5">Total</span>
         </h4>
         <ul className="flex flex-col gap-[49px] md:gap-[18px] list-none">
-          {itemList.map((item, index) => (
+          {fields.map((field, index) => (
             <li key={index} className=" group">
-              <Item {...item} removeItem={removeItem} index={index}></Item>
+              <Item
+                key={field.id}
+                index={index}
+                register={register}
+                errors={errors}
+                remove={remove}
+              ></Item>
             </li>
           ))}
         </ul>
-        <Button onClick={addItem} type="3">
+        <Button
+          type="button"
+          onClick={() => append({ name: "", quantity: "", price: "" })}
+          $type="3"
+        >
           + Add New Item
         </Button>
       </div>
