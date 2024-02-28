59b4794243560f3b5d24f3c882f0f5ee142308aa
Abrham007 <abrhamaraya111@gmail.com>
2024-02-28 08:50:38 +0300

    improved the UX of the error condition

--- src/components/UI/Inputs/InputText.jsx
+++ src/components/UI/Inputs/InputText.jsx
@@ -6,11 +6,19 @@ export default function InputText({
   name,
   ...props
 }) {
+  let labelStyles = "text-sm text-7 dark:text-5";
+  let inputStyles =
+    "w-full pt-[18px] px-5 pb-[15px] text-base text-8 dark:text-white rounded bg-white dark:bg-3 outline-none border border-5 dark:border-[#252945] hover:border-[#9277FF] cursor-pointer";
+
+  if (Object.keys(errors).length !== 0) {
+    labelStyles += " text-9";
+    inputStyles += " border-9";
+  }
   return (
     <label className="w-full flex flex-col gap-[9px] cursor-pointer">
       <p className="w-full flex justify-between">
-        <span className="text-sm text-7 dark:text-5">{label}</span>
-        {errors[name] && (
+        <span className={labelStyles}>{label}</span>
+        {Object.keys(errors).length !== 0 && (
           <span className="text-[0.625rem] font-semibold leading-[1.5] text-9">
             can't be empty
           </span>
@@ -19,7 +27,7 @@ export default function InputText({
 
       <input
         {...register(`${name}`, { required: true })}
-        className="w-full pt-[18px] px-5 pb-[15px] text-base text-8 dark:text-white rounded bg-white dark:bg-3 outline-none border border-5 dark:border-[#252945] hover:border-[#9277FF] cursor-pointer"
+        className={inputStyles}
         type={type}
         {...props}
       ></input>
