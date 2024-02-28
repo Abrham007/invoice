65c318a67811a6455aa51ff25a46bb577c6a4e11
Abrham007 <abrhamaraya111@gmail.com>
2024-02-28 21:31:01 +0300

    improved the logic and made the parent leaner

--- src/components/AddEditInvoice/ErrorList.jsx
+++ src/components/AddEditInvoice/ErrorList.jsx
@@ -1,15 +1,24 @@
-export default function ErrorList({ errors, errorItems }) {
+export default function ErrorList({ errors }) {
+  let hasEmptyField = Object.keys(errors).length !== 0;
+
+  if (Object.keys(errors).length === 1) {
+    if (Object.keys(errors)[0] === "items") {
+      hasEmptyField = false;
+    }
+  }
+
+  let hasNoItem = errors?.items;
   return (
     <ul>
       <li>
-        {Object.keys(errors).length !== 0 && (
+        {hasEmptyField && (
           <p className="text-[0.625rem] font-semibold leading-[1.5] text-9">
             - All fields must be added
           </p>
         )}
       </li>
       <li>
-        {errorItems && (
+        {hasNoItem && (
           <p className="text-[0.625rem] font-semibold leading-[1.5] text-9">
             - An item must be added
           </p>
