5b881e37a0988401171673631409fa6691f980b7
Abrham007 <abrhamaraya111@gmail.com>
2024-02-29 17:15:32 +0300

    added the update and delete state functions

--- src/store/DataContext.js
+++ src/store/DataContext.js
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useReducer, useState } from "react";
-import { fetchInvoices, postInvoice } from "../http";
+import { deleteInvoice, fetchInvoices, postInvoice, putInvoice } from "../http";
 
 export const DataContext = createContext();
 
@@ -7,10 +7,48 @@ export default function DataContextProvider({ children }) {
   const [data, setData] = useState([]);
   const [error, setError] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const [isCreating, setIsCreating] = useState(false);
 
   async function createInvoice(newInvoice) {
-    const createdInvoice = await postInvoice(newInvoice);
-    setData((prevValue) => [...prevValue, createdInvoice]);
+    try {
+      setIsCreating(true);
+      const createdInvoice = await postInvoice(newInvoice);
+      setData((prevValue) => [createdInvoice, ...prevValue]);
+    } catch (error) {
+      setError({ message: error.message || "Failed to create invoice" });
+    }
+    setIsCreating(false);
+  }
+
+  async function updateInvoice(id, updatedInvoice) {
+    setData((prevValue) => {
+      let tempValue = [...prevValue];
+      let updatedInvoiceIndex = tempValue.findIndex(
+        (invoice) => invoice.id === id
+      );
+      tempValue[updatedInvoiceIndex] = { id: id, ...updatedInvoice };
+      return tempValue;
+    });
+
+    try {
+      await putInvoice(id, updatedInvoice);
+    } catch (error) {
+      setData(data);
+      setError({ message: error.message || "Failed to update invoice" });
+    }
+  }
+
+  async function removeInvoice(id) {
+    setData((prevValue) =>
+      [...prevValue].filter((invoice) => invoice.id !== id)
+    );
+
+    try {
+      await deleteInvoice(id);
+    } catch (error) {
+      setData(data);
+      setError({ message: error.message || "Failed to delete invoice" });
+    }
   }
 
   useEffect(() => {
@@ -20,7 +58,7 @@ export default function DataContextProvider({ children }) {
         let invoiceArray = await fetchInvoices();
         setData(invoiceArray);
       } catch (error) {
-        setError({ message: error.message });
+        setError({ message: error.message || "Failed to fetch" });
       }
 
       setIsLoading(false);
@@ -33,6 +71,10 @@ export default function DataContextProvider({ children }) {
     data,
     isLoading,
     error,
+    createInvoice,
+    isCreating,
+    updateInvoice,
+    removeInvoice,
   };
 
   return (
