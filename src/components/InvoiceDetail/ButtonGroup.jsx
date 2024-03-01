727347371852635bc52998caf8f80b19303067db
Abrham007 <abrhamaraya111@gmail.com>
2024-03-01 17:23:32 +0300

    finished the markpaid functionalty

--- src/components/InvoiceDetail/ButtonGroup.jsx
+++ src/components/InvoiceDetail/ButtonGroup.jsx
@@ -1,11 +1,18 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Button from "../UI/Buttons/Button";
 import Modal from "../Modal";
 import Delete from "../Delete";
 import AddEditInvoice from "../AddEditInvoice/AddEditInvoice";
+import { useContext } from "react";
+import { DataContext } from "../../store/DataContext";
+
 export default function ButtonGroup({ id }) {
+  const { data, updateInvoice } = useContext(DataContext);
   const [isEditOpen, setEditOpen] = useState(false);
   const [isDeleteOpen, setDeleteOpen] = useState(false);
+
+  let currentInvoice = data.find((invoice) => invoice.id === id);
+  const isPaid = currentInvoice.status === "paid";
 
   function openEditInvoice() {
     setEditOpen(true);
@@ -13,6 +20,11 @@ export default function ButtonGroup({ id }) {
 
   function openDelete() {
     setDeleteOpen(true);
+  }
+
+  function handleStatusChange() {
+    let newInvoice = { ...currentInvoice, status: "paid" };
+    updateInvoice(id, newInvoice);
   }
 
   return (
@@ -23,7 +35,9 @@ export default function ButtonGroup({ id }) {
       <Button $type="5" onClick={openDelete}>
         Delete
       </Button>
-      <Button $type="2">Mark as Paid</Button>
+      <Button $type="2" disabled={isPaid} onClick={handleStatusChange}>
+        Mark as Paid
+      </Button>
       <Modal
         isOpen={isDeleteOpen}
         setIsOpen={setDeleteOpen}
