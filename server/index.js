60ffebda8215143121a819e82b5ba8c53f78a1d1
Abrham007 <abrhamaraya111@gmail.com>
2024-03-02 14:30:06 +0300

    finished and ready to deploy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import cors from "cors";
 import invoiceRoute from "./Routes/invoiceRoute.js";
-
 import config from "./config.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -10,6 +14,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", invoiceRoute);
+
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
+});
 
 app.listen(config.port, () =>
   console.log(`Server is live @ ${config.hostUrl}`)
